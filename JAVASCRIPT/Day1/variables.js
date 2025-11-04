50252edbc90e8d321f232453f001003dcf5ea39c
Anu-harish <anuacharanu22@gmail.com>
2025-11-04 15:55:56 +0530

    let and const variables

---
+++ JAVASCRIPT/Day1/variables.js
@@ -0,0 +1,21 @@
+// let keyword
+"use strict";
+let message;
+message='Hello';
+message='world'; //value changed
+console.log(message);
+
+
+// constant keyword--we can not reassign the values
+"use strict";
+const num=10;
+console.log(num);
+// num=22;
+
+let msg1="hello world";
+let msg2;
+msg2=msg1;
+console.log(msg1);
+console.log(msg2);
+
+
