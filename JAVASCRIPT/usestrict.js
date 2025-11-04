1020168d7030a3d71fc086ae88a849c6ef46bf68
Anu-harish <anuacharanu22@gmail.com>
2025-11-04 15:38:46 +0530

    use strict keyword

--- JAVASCRIPT/usestrict.js
+++ JAVASCRIPT/usestrict.js
@@ -1,10 +1,18 @@
 // using .it helps catch silent error early
-"use strict";
+// "use strict";
 
-let x = 10; //execute
-console.log(x);
+// let x = 10; //execute
+// console.log(x);
 
 
 //  without using
-x=10; //throws an error .it prevents undeclared variables strictly
-console.log(x); 
+// x=10; //throws an error .it prevents undeclared variables strictly
+// console.log(x); 
+
+
+"use strict";
+
+let x = 10;
+let y = 20; 
+
+console.log(x + y);
