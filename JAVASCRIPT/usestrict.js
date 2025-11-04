c9f4e758ab8b6aedcc334b69754976f8c2ecbf60
Anu-harish <anuacharanu22@gmail.com>
2025-11-04 15:34:26 +0530

    usestrict

---
+++ JAVASCRIPT/usestrict.js
@@ -0,0 +1,10 @@
+// using .it helps catch silent error early
+"use strict";
+
+let x = 10; //execute
+console.log(x);
+
+
+//  without using
+x=10; //throws an error .it prevents undeclared variables strictly
+console.log(x); 
